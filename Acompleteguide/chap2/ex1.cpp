60e676b39dfc0441af437e60de4282bbb0ca916a
Karim-Mttk <89412432+karim-mttk@users.noreply.github.com>
2022-12-25 10:56:23 +0100

    Add files via upload

---
+++ Acompleteguide/chap2/ex1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+cout << "Size of fundamental types\n" << endl;
+
+cout << "char:  "<< sizeof(char)<<endl;
+cout << "short:  "<< sizeof(short)<<endl;
+cout << "int:  "<< sizeof(int)<<endl;
+cout << "long:  "<< sizeof(long)<<endl;
+cout << "float:  "<< sizeof(float)<<endl;
+cout << "double:  "<< sizeof(double)<<endl;
+cout << "long double:  "<< sizeof(long double)<<endl;
+
+return 0;
+
+}
