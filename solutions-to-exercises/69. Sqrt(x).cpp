7865e933a42503538260101dba6eeab4e30d6e67
Karim-Mttk <89412432+karim-mttk@users.noreply.github.com>
2022-12-25 09:16:18 +0100

    Update 69. Sqrt(x).cpp

--- solutions-to-exercises/69. Sqrt(x).cpp
+++ solutions-to-exercises/69. Sqrt(x).cpp
@@ -5,3 +5,27 @@ public:
         return res;
     }
 };
+
+//2nd solution
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low = 1;
+        int high = x;
+        int res = 0;
+
+        while(low<=high){
+            long long int mid = low + (high - low)/2;
+            if(mid*mid<=x){
+                res = mid;
+                low = mid +1;
+            }
+            else 
+            high = mid -1;
+        }
+        return res;
+    }
+}
+
+
