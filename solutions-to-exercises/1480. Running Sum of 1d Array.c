684dadbfd9e63ffadd3501c6843df2674f68f241
Karim-Mttk <89412432+karim-mttk@users.noreply.github.com>
2023-01-04 22:14:02 +0100

    Update 1480. Running Sum of 1d Array.c

--- solutions-to-exercises/1480. Running Sum of 1d Array.c
+++ solutions-to-exercises/1480. Running Sum of 1d Array.c
@@ -8,4 +8,11 @@ public:
         }
         return num;
     }
+    
+    //this is a faster version. 
+      vector<int> runningSum(vector<int>& a) {
+        for( int i=1 ; i<a.size() ; i++ )
+            a[i] += a[i-1] ;
+        return a ;
+    }
 };
