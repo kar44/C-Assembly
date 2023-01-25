7dcc0e61f99aa2e147cda24656c6f4894da15e8f
Karim-Mttk <89412432+karim-mttk@users.noreply.github.com>
2023-01-25 22:39:35 +0100

    Create 35. Search Insert Position.cpp

---
+++ solutions-to-exercises/35. Search Insert Position.cpp
@@ -0,0 +1,8 @@
+  int searchInsert(vector<int>& nums, int target) {
+        int i;
+        for(i = 0; i<nums.size(); i++){
+            if(nums[i]>=target)
+            break;
+        }
+    return i;
+    }
