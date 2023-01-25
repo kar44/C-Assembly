8a9a096e575d86599178b061ec229d135f672c47
Karim-Mttk <89412432+karim-mttk@users.noreply.github.com>
2023-01-25 19:37:30 +0100

    Create 278. First Bad Version(binary search).cpp

---
+++ solutions-to-exercises/278. First Bad Version(binary search).cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int l = 1;
+        int r = n;
+        int m;
+        while(l<r){
+            m = l +(r-l)/2;
+            if(isBadVersion(m)){r = m;}
+            else{l=m+1;}
+        }
+        return l;
+    }
+};
