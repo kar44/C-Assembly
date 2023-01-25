87ce75bd67d97e9d5df0e873b1d26be57c738ffc
Karim-Mttk <89412432+karim-mttk@users.noreply.github.com>
2023-01-25 19:17:07 +0100

    Create 704. Binary Search.cpp

---
+++ solutions-to-exercises/704. Binary Search.cpp
@@ -0,0 +1,6 @@
+  int search(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) 
+            if (nums[i] == target) 
+                return i;
+        return -1;
+    }
